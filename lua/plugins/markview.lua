10ca4f9afe351b8eccd9dcd2bacc12b3d1eeb6b6
Denzyl Dharma <denzyl.dharma@origin.com.au>
2026-04-08 16:28:40 +1000

    Changed markdown plugin
    from render-markdown -> markview

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,10 +1,8 @@
 return {
 	{
-		"MeanderingProgrammer/render-markdown.nvim",
+		"OXY2DEV/markview.nvim",
 		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 		ft = { "markdown" },
-		---@module 'render-markdown'
-		---@type render.md.UserConfig
 		opts = {},
 	},
 }
