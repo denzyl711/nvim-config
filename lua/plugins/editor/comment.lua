ad518feb97be281c71e2f93fcb95d7e365b5ee1a
Denzyl Dharma <denzyl.dharma@origin.com.au>
2026-05-08 11:24:31 +1000

    updated commenting keybinding

--- lua/plugins/editor/comment.lua
+++ lua/plugins/editor/comment.lua
@@ -2,11 +2,15 @@ return {
 	{
 		"numToStr/Comment.nvim",
 		keys = {
-			{ "gc", mode = { "n", "v" }, desc = "Comment toggle" },
-			{ "gb", mode = { "n", "v" }, desc = "Comment toggle blockwise" },
+			{ "<M-/>", mode = { "n", "v" }, desc = "Comment toggle" },
 		},
 		config = function()
-			require("Comment").setup()
+			require("Comment").setup({
+				mappings = { basic = false, extra = false },
+			})
+			local api = require("Comment.api")
+			vim.keymap.set("n", "<M-/>", api.toggle.linewise.current, { desc = "Comment toggle line" })
+			vim.keymap.set("v", "<M-/>", "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", { desc = "Comment toggle selection" })
 		end,
 	},
 }
