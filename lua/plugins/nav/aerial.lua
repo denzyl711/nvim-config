88cec301fddc73cf6c4cee89d876409c5d2c65fe
Denzyl Dharma <denzyl.dharma@origin.com.au>
2026-05-08 11:37:15 +1000

    improved aerial conf

--- lua/plugins/nav/aerial.lua
+++ lua/plugins/nav/aerial.lua
@@ -5,7 +5,21 @@ return {
         "nvim-tree/nvim-web-devicons",
     },
     keys = {
-        { "<leader>a", "<cmd>AerialToggle!<cr>", desc = "Toggle Aerial" },
+        {
+            "<leader>a",
+            function()
+                require("aerial").toggle()
+                if require("aerial").is_open() then
+                    require("aerial").focus()
+                end
+            end,
+            desc = "Toggle Aerial (focus)",
+        },
+        {
+            "<leader>A",
+            "<cmd>AerialToggle!<cr>",
+            desc = "Toggle Aerial (no focus)",
+        },
     },
     opts = {
         layout = {
@@ -13,5 +27,7 @@ return {
             default_direction = "right",
         },
         filter_kind = false,
+        -- Close aerial and jump to selected symbol
+        close_on_select = true,
     },
 }
