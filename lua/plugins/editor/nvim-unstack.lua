49d3fb367983d1347181692b55da6b7a7c07065e
Denzyl Dharma <denzyl.dharma@origin.com.au>
2026-06-17 16:43:13 +1000

    Integrate nvim-unstack with fzf-lua picker
    - Close floaterm before parsing stack trace
    - Show results in fzf-lua dropdown instead of quickfix buffer
    - Yank selection to clipboard for parsing after floaterm closes

--- lua/plugins/editor/nvim-unstack.lua
+++ lua/plugins/editor/nvim-unstack.lua
@@ -3,9 +3,31 @@ return {
 	event = "VeryLazy",
 	version = "*",
 	opts = {
-		debug = false, -- Disable debug logging for performance
-		showsigns = true, -- Disable signs to reduce visual overhead
-		layout = "quickfix_list", -- Use tab layout (default, but explicit for clarity)
-		mapkey = "<leader>s", -- Keep default mapping
+		debug = false,
+		showsigns = true,
+		layout = "quickfix_list",
+		mapkey = false, -- We'll define our own mapping below
 	},
+	config = function(_, opts)
+		require("nvim-unstack").setup(opts)
+
+		-- Wrapper: run unstack, then open quickfix in fzf-lua
+		local function unstack_to_fzf()
+			-- Yank the visual selection to system clipboard
+			vim.cmd('normal! "+y')
+			-- Hide floaterm if any is open
+			pcall(function() vim.cmd("FloatermHide") end)
+			-- Run unstack from clipboard
+			vim.cmd("UnstackFromClipboard")
+			-- Close the quickfix window if it opened, then show fzf picker
+			vim.defer_fn(function()
+				vim.cmd("cclose")
+				if #vim.fn.getqflist() > 0 then
+					require("fzf-lua").quickfix()
+				end
+			end, 10)
+		end
+
+		vim.keymap.set("v", "<leader>s", unstack_to_fzf, { desc = "Unstack to fzf" })
+	end,
 }
